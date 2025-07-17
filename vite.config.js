5faa1b31481e0958fb24129ada0375875b0d734a
Jimmy Huang <huangjimmy@google.com>
2025-07-17 13:42:13 +0800

    Update code

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Nursing-scheduling-system/', // ←這裡放你的 repo 名稱
+  base: '/Nursing-scheduling-system/', 
   plugins: [react()],
 })
 
