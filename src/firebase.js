211376d6240bfe8903df019adcfbb3e77abfb272
JimmyHuang1983 <jay198377@gmail.com>
2025-08-18 13:16:14 +0800

    update firebase auth and deploy.yml bug fix

--- src/firebase.js
+++ src/firebase.js
@@ -4,10 +4,10 @@
 
     // 您的 Firebase 專案設定 (從 Firebase Console 複製)
     const firebaseConfig = {
-      apiKey: import.meta.VITE_API_KEY,
+      apiKey: import.meta.env.VITE_API_KEY,
       authDomain: import.meta.env.VITE_AUTH_DOMAIN,
       projectId: import.meta.env.VITE_PROJECT_ID,
-      storageBucket: import.meta.VITE_STORAGE_BUCKET,
+      storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
       messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
       appId: import.meta.env.VITE_APP_ID,
       measurementId: import.meta.env.VITE_MEASUREMENT_ID,
